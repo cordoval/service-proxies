d912f5c6dc465413ea6d1d113ee2b96e74999fa0
Luis Cordova <cordoval@gmail.com>
2012-11-11 00:32:01 -0500

    final

--- test.php
+++ test.php
@@ -6,22 +6,20 @@ require __DIR__.'/vendor/autoload.php';
 
 $examples = new Examples();
 
-$start = microtime(true);
-$examples->nastyExample();
-echo 'nasty ........ ' . (microtime(true) - $start) . "\n";
+$exampleNames = array(
+    'nastyExample',
+    'goodExample',
+    'goodAndFastExample',
+    'goodAndFastAndAutomaticExample',
+    'pimpleRefactorExample'
+);
 
-$start = microtime(true);
-$examples->goodExample();
-echo 'good ........ ' . (microtime(true) - $start) . "\n";
+timeExample($exampleNames, $examples);
 
-$start = microtime(true);
-$examples->goodAndFastExample();
-echo 'good and fast ... ' . (microtime(true) - $start) . "\n";
-
-$start = microtime(true);
-$examples->goodAndFastAndAutomaticExample();
-echo 'good and fast and automatic ' . (microtime(true) - $start) . "\n";
-
-$start = microtime(true);
-$examples->pimpleRefactorExample();
-echo 'pimple refactor ' . (microtime(true) - $start) . "\n";
+function timeExample($methodNames, $examples) {
+    foreach ($methodNames as $methodName) {
+        $start = microtime(true);
+        call_user_func(array($examples, $methodName));
+        echo $methodName . '........ ' . (microtime(true) - $start) . "\n";
+    }
+}
